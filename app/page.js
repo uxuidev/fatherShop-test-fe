917cec53b37bf9df62f8eb3896ef5dbb31dcf13e
adeelrauf <adeelrauf@gmail.com>
2024-03-17 22:28:40 +0500

    built error fixed

--- app/page.js
+++ app/page.js
@@ -1,14 +1,8 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { redirect } from 'next/navigation'
 import '@fontsource/noto-sans';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => { //Redirect to create account page when the component mounts
-    router.push('/account/create');
-  }, []);
-
-  //nothing to return since it redirects
+  redirect('/account/create');
+  
   //this functionality can also be achieved by setting up redirect in next.config file
 }
