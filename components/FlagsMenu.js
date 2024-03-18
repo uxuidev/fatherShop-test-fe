7602b8b526dc9914e82b1d379138073ec97b6e47
adeelrauf <adeelrauf@gmail.com>
2024-03-18 05:45:26 +0500

    enhanced ux on flags hover

--- components/FlagsMenu.js
+++ components/FlagsMenu.js
@@ -8,8 +8,7 @@ const FlagsMenu = ({ flags, setSelectedFlag }) => {
     <div className={styles['flags-menu']}>
       {
         flags.map(flag => (
-          <div className={styles['flags']} key={flag.id} onClick={()=>setSelectedFlag(flag)}
-          /* style={{backgroundColor: id===flag.id? "#D9D9D9":"E9E9E9"}} onMouseEnter={()=>setId(flag.id)} onMouseLeave={()=>setId(null)} */>
+          <div className={styles['flags']} key={flag.id} onClick={()=>setSelectedFlag(flag)}>
             <img src={flag.src} width="30" height="20" />
             <span className='p-1'>{flag.A2}</span>
             <span style={{ lineHeight: '30px' }}>{flag.code}</span>
